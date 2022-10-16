53f30b1fa2ff5f56e6f4c8673ff80bc49d3f58b9
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-10-16 17:27:32 -0300

    secondycurso laravel sail finalizado

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\XdebugController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('xdebug', [XdebugController::class, 'xdebug']);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
